86aee748b958be28f81ab1d347976a8cf274bffb
Nihaar75858 <nihar.adhiep@gmail.com>
2026-01-09 10:07:26 +0530

    Added Validation to AmountDto

--- src/wallet/dto/amount.dto.ts
+++ src/wallet/dto/amount.dto.ts
@@ -1,4 +1,10 @@
+// Added validation to AmountDto to avoid taking Negative or String values for Amount
+import { IsString, IsNumber, IsPositive } from "class-validator";
 export class AmountDto {
+  @IsString()
   userId: string;
+
+  @IsPositive()
+  @IsNumber()
   amount: number;
 }
